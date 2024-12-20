73cfc1b0db8f4a569fbf06ed564c35ad4e1af306
randomusert <127417963+randomusert@users.noreply.github.com>
2024-12-20 13:57:57 +0200

    random stuff to try getting the crate published

--- src/randomusert_lib/server.rs
+++ src/randomusert_lib/server.rs
@@ -1,6 +1,7 @@
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Request, Response, Server};
 use std::net::SocketAddr;
+use hyper::Server;
 
 pub mod server {
 
--- src/randomusert_lib/sys.rs
+++ src/randomusert_lib/sys.rs
@@ -1,6 +1,7 @@
 use fancy_sys_info::run;
 
 pub mod sys {
+    use fancy_sys_info::run;
     fn sys() {
         if let Err(err) = run() {
             eprintln!("Error: {}", err);
